0435df32860a428039b44ac5b90d0c9d7e6b8981
skipper703 <69033504+skipper703@users.noreply.github.com>
2020-08-02 15:47:45 +0300

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,13 +1,15 @@
+""" Programm are finding all files in directory 'main' that ends '.py' and write it in answer.txt file from new line everytime"""
+
 import os, os.path
-w = open("answer.txt", "w")
-print(os.getcwd())
-os.chdir("test")
-lst = []
-for current_dir, dirs, files in os.walk("."):
-    for file_name in files:
-        if file_name.endswith(".py"):
-            if current_dir not in lst:
-                lst.append(current_dir)
-for i in range(len(lst)):
-    w.write(lst[i])
-    w.write('\n')
+with w = open("answer.txt", "w"):
+    print(os.getcwd())
+    os.chdir("test")
+    lst = []
+    for current_dir, dirs, files in os.walk("."):
+        for file_name in files:
+            if file_name.endswith(".py"):
+                if current_dir not in lst:
+                    lst.append(current_dir)
+    for i in range(len(lst)):
+        w.write(lst[i])
+        w.write('\n')
