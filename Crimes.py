cae3136bf4e58d0fa91ebdfd3701492858cb65aa
skipper703 <69033504+skipper703@users.noreply.github.com>
2020-08-07 16:31:52 +0300

    Add files via upload

---
+++ Crimes.py
@@ -0,0 +1,18 @@
+import csv
+
+def get_key(d, value):
+    for k, v in d.items():
+        if v == value:
+            return k
+
+lst = {}
+with open("D:\Code\Stepik\example.csv", 'r') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        if row[5] not in lst:
+            lst[row[5]] = 0
+        else:
+            lst[row[5]] += 1
+
+print(get_key(lst, max(lst.values())))
+
